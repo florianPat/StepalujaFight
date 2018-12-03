0abb1116582535c1ddc123513f8f8a6295172468
florianPat <florian.patruck@gmx.de>
2018-12-03 15:00:01 +0100

    Added a DatagramChannel and a Selector for non-blocking socket I/O

--- core/src/de/patruck/stepaluja/GameServerLobbyLevel.java
+++ core/src/de/patruck/stepaluja/GameServerLobbyLevel.java
@@ -4,8 +4,14 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.io.IOException;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
+import java.util.Set;
 
 public class GameServerLobbyLevel extends LoadingLevel
 {
@@ -18,6 +24,11 @@ public class GameServerLobbyLevel extends LoadingLevel
     private String clientExternalIp = null;
     private String clientExternalPort = null;
     private String clientLocalIp = null;
+    private DatagramChannel channel = null;
+    private Selector selector = null;
+    byte[] bufferBytes;
+    private ByteBuffer readBuffer;
+    private ByteBuffer writeBuffer;
     private float packetInterval = 0.0f;
     public static final float maxPacketInterval = 2.0f;
 
@@ -26,6 +37,10 @@ public class GameServerLobbyLevel extends LoadingLevel
         super(screenManager, worldSize);
 
         msg = "Created a server, because there where no open ones!\nWaiting for players to join...";
+
+        readBuffer = ByteBuffer.allocate(256);
+        writeBuffer = ByteBuffer.allocate(256);
+        bufferBytes = new byte[256];
     }
 
     @Override
@@ -36,6 +51,7 @@ public class GameServerLobbyLevel extends LoadingLevel
         showingUpInDB = false;
         isClientConnecting = false;
 
+        //TODO: Think about not getting the address every time, just once on startup!
         String ipAddress;
         int port;
         String localIpAddress;
@@ -51,22 +67,23 @@ public class GameServerLobbyLevel extends LoadingLevel
 //            Utils.invalidCodePath();
 //        }
 
-        DatagramSocket socket = null;
         try
         {
-            socket = new DatagramSocket();
+            channel = DatagramChannel.open();
         }
-        catch(SocketException e)
+        catch(IOException e)
         {
             e.printStackTrace();
             Utils.invalidCodePath();
         }
+        Utils.aassert(channel != null);
+        DatagramSocket socket = channel.socket();
         try
         {
             Utils.aassert(socket != null);
             socket.connect(new InetSocketAddress("google.com", localPort));
         }
-        catch(IOException e)
+        catch(Exception e)
         {
             e.printStackTrace();
             Utils.invalidCodePath();
@@ -77,6 +94,66 @@ public class GameServerLobbyLevel extends LoadingLevel
         localPortHere = socket.getLocalPort();
         socket.disconnect();
         socket.close();
+        try
+        {
+            channel.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        try
+        {
+            channel = DatagramChannel.open();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        Utils.aassert(channel != null);
+        socket = channel.socket();
+        try
+        {
+            socket.bind(new InetSocketAddress(localIpAddress, localPort));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        Utils.aassert(socket.isBound());
+        try
+        {
+            channel.configureBlocking(false);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        Utils.aassert(!channel.isBlocking());
+
+        try
+        {
+            selector = Selector.open();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        Utils.aassert(selector != null);
+        try
+        {
+            channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
 
         NativeBridge.registerNewServer(ipAddress + "_" + port + "-" + localIpAddress);
     }
@@ -139,7 +216,82 @@ public class GameServerLobbyLevel extends LoadingLevel
         }
         else
         {
+            int nToSelect = 0;
             Utils.log("Now its communicating time!");
+            try
+            {
+                nToSelect = selector.selectNow();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                Utils.invalidCodePath();
+            }
+
+            if(nToSelect > 0)
+            {
+                Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+
+                for(Iterator<SelectionKey> iterator = selectionKeySet.iterator(); iterator.hasNext(); )
+                {
+                    SelectionKey it = iterator.next();
+
+                    if(it.isReadable())
+                    {
+                        //NOTE: The channel really has to be the channel of the class, because we just
+                        //registered one!
+                        DatagramChannel channel = (DatagramChannel) it.channel();
+
+                        InetSocketAddress address = null;
+                        try
+                        {
+                            address = (InetSocketAddress) channel.receive(readBuffer);
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                            Utils.invalidCodePath();
+                        }
+                        Utils.aassert(address != null);
+
+                        Utils.log("Received a packet from:" + address.getHostName());
+
+                        readBuffer.get(bufferBytes);
+                    }
+                    if(it.isWritable())
+                    {
+                        //NOTE: The channel really has to be the channel of the class, because we just
+                        //registered one!
+                        DatagramChannel channel = (DatagramChannel) it.channel();
+
+                        InetAddress inetAddress = null;
+                        try
+                        {
+                            inetAddress = InetAddress.getByName(clientLocalIp);
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                            Utils.invalidCodePath();
+                        }
+                        Utils.aassert(inetAddress != null);
+                        InetSocketAddress socketAddress = new InetSocketAddress(inetAddress, localPort);
+                        try
+                        {
+                            channel.send(writeBuffer, socketAddress);
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                            Utils.invalidCodePath();
+                        }
+
+                        Utils.log("We send a msg!");
+                    }
+
+                    iterator.remove();
+                }
+            }
         }
     }
 
@@ -219,7 +371,24 @@ public class GameServerLobbyLevel extends LoadingLevel
 
         unregisterDBAndListener();
 
-        //TODO: Close server!
+        try
+        {
+            channel.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
+        try
+        {
+            selector.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Utils.invalidCodePath();
+        }
     }
 
     @Override
